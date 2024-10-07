35589ae1d6f6ea8f839b37ccb960af6c6d60fe5e
Nevena Jakovljevic <nevena.jakovljevic@grape.ba>
2024-10-07 14:57:27 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+
+
